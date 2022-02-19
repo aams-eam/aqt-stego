e934a48a7241cd9e802980f0c91dd1bf62461fef
alexauf <mrAlexTFB@gmail.com>
2022-02-19 23:36:48 +0100

    Web page created

---
+++ stego/mainPage/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files import File
+import os
+
+
+# FUNCTIONS
+def writetofile(content, filedir):
+    f = open(os.getcwd()+filedir, 'w')
+    testfile = File(f)
+    testfile.write(content)
+    testfile.close
+    f.close
+    return HttpResponse()
+
+
+# Create your views here.
+def home(request):
+
+    # CODE INDEX MANIPULATION
+
+
+    response =  render(request, 'mainPage/indexExpanded.html')
+
+    print()
+    print()
+    print()
+    print("comment  ")
+    print()
+    print()
+    print()
+
+    writetofile(response.content.decode(response.charset), "responseContent.html")
+
+    # with open("responseContent.html", 'w') as fd:
+    #     fd.write(response.content.decode(response.charset))
+
+    return response
+
+
+
+from django.http import HttpResponse
+
+# Create your views here.
