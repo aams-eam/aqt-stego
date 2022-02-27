0645c04c2e196222cc3a3e5631265c70d454162a
alexauf <mrAlexTFB@gmail.com>
2022-02-27 12:25:16 +0100

    Created example of encryption and embedded it in django web-sever, but need to change the dynamic as now the encoding is done with the POST and must be done with the GET as here is where we are receiving the public Key

--- stego/mainPage/pythonScripts/encryptionExample.py
+++ stego/mainPage/pythonScripts/encryptionExample.py
@@ -1,35 +1,39 @@
-import sslcrypto
-from Crypto.Cipher import ARC4
-from Crypto.Hash import SHA
-from Crypto.Random import get_random_bytes
-
-
-msg = b'stego message'
-
-# generate session key of 160 bits
-random = get_random_bytes(16)
-session_key = SHA.new(random).digest() # 160 bits key length
-
-# cipher with session key
-cipher = ARC4.new(session_key)
-encmsg = cipher.encrypt(msg)
-print("MSG:", len(msg), msg)
-print("CIPHERMESSAGE:", len(encmsg), encmsg)
-
-# Create curve object, generate private and public keys
-curve = sslcrypto.ecc.get_curve("secp128r1")
-private_key = curve.new_private_key(is_compressed=True)
-public_key = curve.private_to_public(private_key)
-
-
-# Encrypt session key
-enckey = curve.encrypt(session_key, public_key, algo="aes-256-ofb")
+# import sslcrypto
+# from Crypto.Cipher import ARC4
+# from Crypto.Hash import SHA
+# from Crypto.Random import get_random_bytes
 #
-# # Decrypt
-# assert curve.decrypt(ciphertext, private_key, algo="aes-256-ofb") == data
+#
+# msg = b'stego message'
+#
+# # generate session key of 160 bits
+# random = get_random_bytes(16)
+# session_key = SHA.new(random).digest() # 160 bits key length
+#
+# # cipher with session key
+# cipher = ARC4.new(session_key)
+# encmsg = cipher.encrypt(msg)
+# print("MSG:", len(msg), msg)
+# print("CIPHERMESSAGE:", len(encmsg), encmsg)
+#
+# # Create curve object, generate private and public keys
+# curve = sslcrypto.ecc.get_curve("secp128r1")
+# private_key = curve.new_private_key(is_compressed=True)
+# public_key = curve.private_to_public(private_key)
+#
+#
+# # Encrypt session key
+# enckey = curve.encrypt(session_key, public_key, algo="aes-256-ofb")
+# #
+# # # Decrypt
+# # assert curve.decrypt(ciphertext, private_key, algo="aes-256-ofb") == data
+#
+#
+# # concatenate the session key ciphered and the message
+# # init = "00010010"
+# total = enckey+encmsg
+# print(len(total), len(enckey), len(encmsg))
 
-
-# concatenate the session key ciphered and the message
-# init = "00010010"
-total = enckey+encmsg
-print(len(total), len(enckey), len(encmsg))
+bits = '10011000'
+lbits = list(bits)
+print(lbits)
