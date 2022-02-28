dde46708e332895303391195a803ee09924558c0
ivantorrejon98 <48805567+ivantorrejon98@users.noreply.github.com>
2022-03-01 00:21:48 +0100

    Update codification_commas.py

--- stego/mainPage/pythonScripts/codification_commas.py
+++ stego/mainPage/pythonScripts/codification_commas.py
@@ -1,11 +1,16 @@
-from .counter_bytes_tags import contadorcomillas
 
-# '''INSERTAR COMILLAS SIMPLES O DOBLES SEGUN ENTRADA'''
+from counter_bytes_tags import contadorcomillas
+
+
+
+'''INSERTAR COMILLAS SIMPLES O DOBLES SEGUN ENTRADA'''
 
 def insertar_comillas(linea_html,entrada_codificada) :
 	indic=0
 	lin=list(linea_html)
 	primer=0
+	primer_simple=0
+	control_simple=0
 	control=0
 	bits_comillas= contadorcomillas(linea_html)
 	for index in range(len(lin)):
@@ -13,7 +18,7 @@ def insertar_comillas(linea_html,entrada_codificada) :
 			if((len(entrada_codificada))!=0):
 				if(indic < bits_comillas):
 					if primer == 0 :
-						if(entrada_codificada[0] == '1'):
+						if(entrada_codificada[0] == '0'):
 							lin[index] = "'"
 							control=1
 							primer=1
@@ -30,8 +35,29 @@ def insertar_comillas(linea_html,entrada_codificada) :
 							primer=0
 							entrada_codificada.pop(0)
 							indic+=1
-	linea_html_comillas=""
+		elif lin[index] == "'":
+			if((len(entrada_codificada))!=0):
+				if(indic < bits_comillas):
+					if primer_simple == 0 :
+						if(entrada_codificada[0] == '1'):
+							lin[index] = '"'
+							control_simple=1
+							primer_simple=1
+						else:
+							primer_simple=1
+					else :
+						if(control_simple == 1):
+							lin[index] = '"'
+							control_simple=0
+							primer_simple=0
+							entrada_codificada.pop(0)
+							indic+=1
+						else:
+							primer_simple=0
+							entrada_codificada.pop(0)
+							indic+=1
+	linea_html_comillas=""	
 	for j in range(len(lin)):
 		linea_html_comillas+=lin[j]
-
+	
 	return linea_html_comillas
