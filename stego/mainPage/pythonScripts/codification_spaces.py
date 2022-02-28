774dbafaf27dee958aab16097b54484f086036c4
ivantorrejon98 <48805567+ivantorrejon98@users.noreply.github.com>
2022-03-01 00:22:57 +0100

    Update codification_spaces.py

--- stego/mainPage/pythonScripts/codification_spaces.py
+++ stego/mainPage/pythonScripts/codification_spaces.py
@@ -1,7 +1,9 @@
-from .counter_bytes_tags import contadortags
 
 
-# '''TAGS SEGUN ENTRADA'''
+from counter_bytes_tags import contadortags
+
+
+'''TAGS SEGUN ENTRADA'''
 
 def insertar_tags(linea_html,entrada_codificada) :
 	inde=0
@@ -15,13 +17,14 @@ def insertar_tags(linea_html,entrada_codificada) :
 					if(entrada_codificada[0] == '1'):
 						lin.insert(index+insertados+1,' ')
 						entrada_codificada.pop(0)
+						insertados+=1
 						inde+=1
 					else:
 						entrada_codificada.pop(0)
 						inde+=1
-		elif (lin[index+insertados] == '>') and (lin[index+insertados-1] != ' '):
+		elif ((lin[index+insertados]) == '>'):
 			if((len(entrada_codificada))!=0):
-				if(inde<len(entrada_codificada)):
+				if(inde<bits_tags):
 					if(entrada_codificada[0] == '1'):
 						lin.insert(index+insertados,' ')
 						entrada_codificada.pop(0)
@@ -30,9 +33,9 @@ def insertar_tags(linea_html,entrada_codificada) :
 					else:
 						entrada_codificada.pop(0)
 						inde+=1
-
+	
 	linea_html_tags=""
 	for j in range(len(lin)):
 		linea_html_tags+=lin[j]
-
+		
 	return linea_html_tags
