a1fb5b580a6f1d11da30ef06c5fd89d16a7dbd15
ivantorrejon98 <48805567+ivantorrejon98@users.noreply.github.com>
2022-03-03 18:42:23 +0100

    Update test.py

--- stego/mainPage/pythonScripts/test.py
+++ stego/mainPage/pythonScripts/test.py
@@ -3,3 +3,5 @@ from remove_line import remove_line_html
 
 with open(os.getcwd()+"\\responseContent.html") as fd:
     content = fd.read()
+html_line_removed=remove_line_html(content)
+print(html_line_removed)
