82e835324b32bfede15957f6368156ecb4a89360
alexauf <mrAlexTFB@gmail.com>
2022-03-04 23:41:22 +0100

    minor changes

---
+++ stego/mainPage/pythonScripts/counter_bytes_tags2.py
@@ -0,0 +1,17 @@
+'''CONTADOR NUMERO DE TAGS EN FICHERO'''
+def contadortags(linestags):
+	contadortags=0
+	lin=list(linestags)
+	for index in range(len(lin)):
+		if (((chr(lin[index])) == '<') or ((chr(lin[index])) == '>')):
+			contadortags+=1
+	return contadortags
+
+'''CONTADOR NUMERO COMILLAS EN FICHERO'''
+def contadorcomillas(linescomillas):
+	contadorcomillas=0
+	lin=list(linescomillas)
+	for index in range(len(lin)):
+		if (((chr(lin[index])) == '"') or ((chr(lin[index])) == "'")):
+			contadorcomillas+=1
+	return int(contadorcomillas/2)
