7d44144be1a6fa491fe3cb98ebf9bee13a21182a
alexauf <mrAlexTFB@gmail.com>
2022-03-04 23:38:59 +0100

    updated statistics.py so graph is apparently working great, need to create csv file (take execution time) and try

--- stego/mainPage/pythonScripts/statistics.py
+++ stego/mainPage/pythonScripts/statistics.py
@@ -1,11 +1,12 @@
 from counter_bytes_tags2 import contadortags
 from counter_bytes_tags2 import contadorcomillas
-from attPosition import total_capacity as count_total_att
+from attPosition import max_bits_line as max_bits_att_line
 import requests
 from requests.exceptions import ConnectionError
 import html
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 try:
     # For Python 3.0 and later
@@ -14,7 +15,31 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
-'''webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/','https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']'''
+
+def find_tags_line (input):
+
+    matches = re.findall(r'<(.*?)>', input)
+    return matches
+
+
+def web_counter_att(webpage_l):
+    webpage_number_tags=[]
+    for elem in webpage_l:
+        r = requests.get(elem)
+        htmlresponse = html.unescape(r.text)
+
+        # expand tags in one line to multiple lines
+        tags = []
+        for line in htmlresponse.splitlines():
+            tags += find_tags_line(line)
+
+        # count bits in attributes base on the expanded lines
+        num = 0
+        for t in tags:
+            complete = "<"+t+">"
+            num += max_bits_att_line(complete)
+
+    return num
 
 
 def web_counter_tags(webpage_l):
@@ -77,6 +102,12 @@ def web_counter_lines(webpage_l):
 	return webpage_lines
 
 
+def test():
+    webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/','https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']
+    x = web_counter_att(webpage_list)
+    print(x)
+
+
 def main():
 
     try:
@@ -95,6 +126,8 @@ def main():
         print("lines counted")
         characters = web_counter_characters(webpage_list)
         print("characters counted")
+        att_cap = web_counter_att(webpage_list)
+        print("attributes counted")
         quotes_cap = web_counter_quotes(webpage_list)
         print("quotes counted")
         tag_cap = web_counter_tags(webpage_list)
@@ -105,7 +138,7 @@ def main():
         print(quotes_cap)
         print(tag_cap)
 
-        df = pd.DataFrame({"page": webpage_list,"characters": characters,"lines": lines, "quotes_cap": quotes_cap, "tag_cap": tag_cap})
+        df = pd.DataFrame({"page": webpage_list,"characters": characters,"lines": lines, "att_cap": att_cap "quotes_cap": quotes_cap, "tag_cap": tag_cap})
         df.to_csv("50topStatistics.csv", index=False)
 
         df = pd.read_csv("50topStatistics.csv")
@@ -113,17 +146,17 @@ def main():
 
 
     df['totalbits'] = df['characters']*8
-    # df['att_percentage'] = 100*(df['att_cap']/df['totalbits'])
+    df['att_percentage'] = 100*(df['att_cap']/df['totalbits'])
     df['quotes_percentage'] = 100*(df['quotes_cap']/df['totalbits'])
     df['tag_percentage'] = 100*(df['tag_cap']/df['totalbits'])
 
     # sort df by totalbits ascending
     df.sort_values('totalbits', ascending=True, inplace=True)
     df.reset_index(drop=True, inplace=True)
-    print(df[['totalbits', 'quotes_percentage', 'tag_percentage']].head())
+    print(df[['totalbits', 'att_percentage', 'quotes_percentage', 'tag_percentage']].head())
 
 
-    # plt.plot(df.index.to_list(), df.att_cap.to_list(), label="att encoding")
+    plt.plot(df.index.to_list(), df.att_percentage.to_list(), label="att encoding")
     plt.plot(df.index.to_list(), df.quotes_percentage.to_list(), label="quote encoding")
     plt.plot(df.index.to_list(), df.tag_percentage.to_list(), label="tag encoding")
     plt.xlabel("Webpage index")
