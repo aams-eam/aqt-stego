2422fb3afd857b5e74aa0654560bde64842dda1c
alexauf <mrAlexTFB@gmail.com>
2022-03-05 10:08:04 +0100

    make modifications so Ivan can better a the functions

--- stego/mainPage/pythonScripts/counter_bytes_tags2.py
+++ stego/mainPage/pythonScripts/counter_bytes_tags2.py
@@ -1,19 +1,14 @@
 '''CONTADOR NUMERO DE TAGS EN FICHERO'''
 def contadortags(linestags):
-<<<<<<< HEAD
+
 	contadortags=0
-=======
-	contadortags=0	
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
+	
 	lin=list(linestags)
 	for index in range(len(lin)):
 		if (((chr(lin[index])) == '<') or ((chr(lin[index])) == '>')):
 			contadortags+=1
-<<<<<<< HEAD
+
 	return contadortags
-=======
-	return contadortags	
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
 
 '''CONTADOR NUMERO COMILLAS EN FICHERO'''
 def contadorcomillas(linescomillas):
--- stego/mainPage/pythonScripts/statistics.py
+++ stego/mainPage/pythonScripts/statistics.py
@@ -1,27 +1,13 @@
 from counter_bytes_tags2 import contadortags
 from counter_bytes_tags2 import contadorcomillas
-<<<<<<< HEAD
-<<<<<<< HEAD
-from attPosition import max_bits_line as max_bits_att_line
-=======
 from attPosition import total_capacity as count_total_att
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
 from attPosition import max_bits_line as max_bits_att_line
->>>>>>> feature-13
 import requests
 from requests.exceptions import ConnectionError
 import html
 import pandas as pd
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-<<<<<<< HEAD
 import re
-=======
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
-import re
->>>>>>> feature-13
 
 try:
     # For Python 3.0 and later
@@ -30,10 +16,6 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> feature-13
 
 def find_tags_line (input):
 
@@ -43,7 +25,9 @@ def find_tags_line (input):
 
 def web_counter_att(webpage_l):
     webpage_number_tags=[]
+    numl = []
     for elem in webpage_l:
+        print(elem)
         r = requests.get(elem)
         htmlresponse = html.unescape(r.text)
 
@@ -58,19 +42,16 @@ def web_counter_att(webpage_l):
             complete = "<"+t+">"
             num += max_bits_att_line(complete)
 
-    return num
-<<<<<<< HEAD
-=======
-'''webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/','https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']'''
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
->>>>>>> feature-13
+        numl.append(num)
+
+    return numl
+
 
 
 def web_counter_tags(webpage_l):
 	webpage_number_tags=[]
 	for elem in webpage_l:
-		'''print(elem)'''
+		print(elem)
 		html = urlopen(elem)
 		ey=html.readlines()
 		tags=0
@@ -81,10 +62,12 @@ def web_counter_tags(webpage_l):
 		webpage_number_tags.append(tags)
 	return webpage_number_tags
 
+
+
 def web_counter_quotes(webpage_l):
 	webpage_number_quotes=[]
 	for elem in webpage_l:
-		'''print(elem)'''
+		print(elem)
 		html = urlopen(elem)
 		ey=html.readlines()
 		tags=0
@@ -95,10 +78,12 @@ def web_counter_quotes(webpage_l):
 		webpage_number_quotes.append(tags)
 	return webpage_number_quotes
 
+
+
 def web_counter_characters(webpage_l):
 	webpage_length=[]
 	for elem in webpage_l:
-		'''print(elem)'''
+		print(elem)
 		html = urlopen(elem)
 		ey=html.readlines()
 		tags=0
@@ -111,10 +96,12 @@ def web_counter_characters(webpage_l):
 		webpage_length.append(size)
 	return webpage_length
 
+
+
 def web_counter_lines(webpage_l):
 	webpage_lines=[]
 	for elem in webpage_l:
-		'''print(elem)'''
+		print(elem)
 		html = urlopen(elem)
 		ey=html.readlines()
 		tags=0
@@ -127,21 +114,14 @@ def web_counter_lines(webpage_l):
 	return webpage_lines
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> feature-13
+
 def test():
-    webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/','https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']
+    webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/']#,'https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']
     x = web_counter_att(webpage_list)
     print(x)
 
 
-<<<<<<< HEAD
-=======
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
->>>>>>> feature-13
+
 def main():
 
     try:
@@ -152,43 +132,40 @@ def main():
 
         print("50topStatistics.csv does not exist, creating...")
 
-        webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/','https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']
+        webpage_list=['https://www.google.com/','https://www.youtube.com/','https://www.facebook.com/']#,'https://twitter.com/','https://www.instagram.com/','http://www.baidu.com/','https://www.wikipedia.org/','https://yandex.ru/','https://es.yahoo.com/','https://www.elmundo.es/','https://www.whatsapp.com/','https://www.netflix.com/es/','https://www.uc3m.es/Inicio','https://vine.co/','https://www.yahoo.co.jp/','https://outlook.live.com/owa/','https://as.com/','https://zoom.us/','https://www.reddit.com/','https://elpais.com/','https://www.office.com/','https://www.spotify.com/es/','https://vk.com/','https://www.hola.com/','https://www.twitch.tv/','https://www.elperiodico.com/es/','https://www.naver.com/','https://www.bing.com/','https://www.roblox.com/','https://duckduckgo.com/','https://www.elespanol.com/','https://mail.ru/','https://www.pinterest.es/','https://www.defensa.gob.es/','https://www.qq.com/','https://news.yahoo.co.jp/','https://www.fandom.com/','https://www.msn.com/es-es/','https://www.google.com.br/','https://www.globo.com/','https://www.ebay.com/','https://www.rtve.es/','https://www.movistar.es/','https://weather.com/es-ES/tiempo/hoy/l/SPXX0050:1:SP?Goto=Redirected','https://ok.ru/','https://ok.ru/','https://www.bbc.com/','https://www.marca.com/','https://www.sport.es/es/','https://www.casadellibro.com/','https://www.w3schools.com/']
 
 
-        print("COUNTING...")
-        lines = web_counter_lines(webpage_list)
-        print("lines counted")
-        characters = web_counter_characters(webpage_list)
-        print("characters counted")
-<<<<<<< HEAD
-<<<<<<< HEAD
-        att_cap = web_counter_att(webpage_list)
-        print("attributes counted")
-=======
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
-        att_cap = web_counter_att(webpage_list)
-        print("attributes counted")
->>>>>>> feature-13
-        quotes_cap = web_counter_quotes(webpage_list)
-        print("quotes counted")
-        tag_cap = web_counter_tags(webpage_list)
-        print("tags counted")
+        # MODIFICAR IVAN
+        lines = []
+        characters = []
+        att_cap = []
+        quotes_cap = []
+        tag_cap = []
 
-        print(characters)
-        print(lines)
-        print(quotes_cap)
-        print(tag_cap)
+        for elem in webpage_list:
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        df = pd.DataFrame({"page": webpage_list,"characters": characters,"lines": lines, "att_cap": att_cap "quotes_cap": quotes_cap, "tag_cap": tag_cap})
-=======
-        df = pd.DataFrame({"page": webpage_list,"characters": characters,"lines": lines, "quotes_cap": quotes_cap, "tag_cap": tag_cap})
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
-        df = pd.DataFrame({"page": webpage_list,"characters": characters,"lines": lines, "att_cap": att_cap "quotes_cap": quotes_cap, "tag_cap": tag_cap})
->>>>>>> feature-13
+            # hacer peticion html
+
+            # calcular estadística solo de ese html
+            print("COUNTING...")
+            lines.append(web_counter_lines(elem))
+            print("lines counted")
+            characters.append(web_counter_characters(elem))
+            print("characters counted")
+            att_cap.append(web_counter_att(elem))
+            print("attributes counted")
+            quotes_cap.append(web_counter_quotes(elem))
+            print("quotes counted")
+            tag_cap.append(web_counter_tags(elem))
+            print("tags counted")
+
+            print(characters)
+            print(lines)
+            print(quotes_cap)
+            print(tag_cap)
+        # MODIFICAR IVAN
+
+        df = pd.DataFrame({"page": webpage_list,"characters": characters,"lines": lines, "att_cap": att_cap, "quotes_cap": quotes_cap, "tag_cap": tag_cap})
         df.to_csv("50topStatistics.csv", index=False)
 
         df = pd.read_csv("50topStatistics.csv")
@@ -196,39 +173,17 @@ def main():
 
 
     df['totalbits'] = df['characters']*8
-<<<<<<< HEAD
-<<<<<<< HEAD
     df['att_percentage'] = 100*(df['att_cap']/df['totalbits'])
-=======
-    # df['att_percentage'] = 100*(df['att_cap']/df['totalbits'])
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
-    df['att_percentage'] = 100*(df['att_cap']/df['totalbits'])
->>>>>>> feature-13
     df['quotes_percentage'] = 100*(df['quotes_cap']/df['totalbits'])
     df['tag_percentage'] = 100*(df['tag_cap']/df['totalbits'])
 
     # sort df by totalbits ascending
     df.sort_values('totalbits', ascending=True, inplace=True)
     df.reset_index(drop=True, inplace=True)
-<<<<<<< HEAD
-<<<<<<< HEAD
     print(df[['totalbits', 'att_percentage', 'quotes_percentage', 'tag_percentage']].head())
 
 
     plt.plot(df.index.to_list(), df.att_percentage.to_list(), label="att encoding")
-=======
-    print(df[['totalbits', 'quotes_percentage', 'tag_percentage']].head())
-
-
-    # plt.plot(df.index.to_list(), df.att_cap.to_list(), label="att encoding")
->>>>>>> 709e1878918df6c248144c86a80140ba774b0561
-=======
-    print(df[['totalbits', 'att_percentage', 'quotes_percentage', 'tag_percentage']].head())
-
-
-    plt.plot(df.index.to_list(), df.att_percentage.to_list(), label="att encoding")
->>>>>>> feature-13
     plt.plot(df.index.to_list(), df.quotes_percentage.to_list(), label="quote encoding")
     plt.plot(df.index.to_list(), df.tag_percentage.to_list(), label="tag encoding")
     plt.xlabel("Webpage index")
@@ -243,3 +198,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # test()
