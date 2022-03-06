91f16bb01f7eb65be008395fea6d6f58d1b9b4f0
ivantorrejon98 <48805567+ivantorrejon98@users.noreply.github.com>
2022-03-06 17:57:52 +0100

    Update remove_line.py

--- stego/mainPage/pythonScripts/remove_line.py
+++ stego/mainPage/pythonScripts/remove_line.py
@@ -4,22 +4,25 @@ import time
 
 def remove_line_html(html,number_lines) :
 	random.seed(time.process_time())
-	lin=list(html)
-	lines=[]
-	for ind in range(number_lines):
-		rand=random.randint(0,len(lin))
-		lines.append(rand)
-
+	lines=[random.randint(0,len(html))] 
+	i=1
+	while i<number_lines:
+		rand=random.randint(0,len(html))
+		if rand not in lines:
+			lines.append(rand)
+			i+=1
 	lines.sort()
 	count=0
 	html_noline=""
-	for index in range(len(lin)):
+	index=0
+	for linea in html:
 		if(count<number_lines):
 			if index!=lines[count] :
-				html_noline+=lin[index]
+				html_noline+=linea
 			else:
 				count+=1
 		else:
-			html_noline+=lin[index]
-
+			html_noline+=linea
+		index+=1
+	
 	return html_noline
